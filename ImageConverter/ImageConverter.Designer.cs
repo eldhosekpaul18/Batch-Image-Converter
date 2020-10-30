2cfec986bfd9c9f8fb52945f437272402bf56c09
Eldhose K Paul <eldhose.k@zerone-consulting.com>
2020-10-30 14:01:57 +0530

    Add project files.

---
+++ ImageConverter/ImageConverter.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace ImageConverter
+{
+    partial class ImageConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInputFolder = new System.Windows.Forms.TextBox();
+            this.txtOutputFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowseInput = new System.Windows.Forms.Button();
+            this.btnBrowseOutput = new System.Windows.Forms.Button();
+            this.chkSearchSubDirectory = new System.Windows.Forms.CheckBox();
+            this.chkKeepStruct = new System.Windows.Forms.CheckBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtOutPut = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbOutputFormat = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Output Folder";
+            // 
+            // txtInputFolder
+            // 
+            this.txtInputFolder.Location = new System.Drawing.Point(227, 55);
+            this.txtInputFolder.Name = "txtInputFolder";
+            this.txtInputFolder.Size = new System.Drawing.Size(307, 26);
+            this.txtInputFolder.TabIndex = 1;
+            // 
+            // txtOutputFolder
+            // 
+            this.txtOutputFolder.Location = new System.Drawing.Point(227, 92);
+            this.txtOutputFolder.Name = "txtOutputFolder";
+            this.txtOutputFolder.Size = new System.Drawing.Size(307, 26);
+            this.txtOutputFolder.TabIndex = 2;
+            // 
+            // btnBrowseInput
+            // 
+            this.btnBrowseInput.Location = new System.Drawing.Point(566, 57);
+            this.btnBrowseInput.Name = "btnBrowseInput";
+            this.btnBrowseInput.Size = new System.Drawing.Size(75, 31);
+            this.btnBrowseInput.TabIndex = 2;
+            this.btnBrowseInput.Text = "Browse";
+            this.btnBrowseInput.UseVisualStyleBackColor = true;
+            this.btnBrowseInput.Click += new System.EventHandler(this.btnBrowseInput_Click);
+            // 
+            // btnBrowseOutput
+            // 
+            this.btnBrowseOutput.Location = new System.Drawing.Point(566, 94);
+            this.btnBrowseOutput.Name = "btnBrowseOutput";
+            this.btnBrowseOutput.Size = new System.Drawing.Size(75, 31);
+            this.btnBrowseOutput.TabIndex = 2;
+            this.btnBrowseOutput.Text = "Browse";
+            this.btnBrowseOutput.UseVisualStyleBackColor = true;
+            this.btnBrowseOutput.Click += new System.EventHandler(this.btnBrowseOutput_Click);
+            // 
+            // chkSearchSubDirectory
+            // 
+            this.chkSearchSubDirectory.AutoSize = true;
+            this.chkSearchSubDirectory.Checked = true;
+            this.chkSearchSubDirectory.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkSearchSubDirectory.Location = new System.Drawing.Point(669, 57);
+            this.chkSearchSubDirectory.Name = "chkSearchSubDirectory";
+            this.chkSearchSubDirectory.Size = new System.Drawing.Size(176, 24);
+            this.chkSearchSubDirectory.TabIndex = 3;
+            this.chkSearchSubDirectory.Text = "Search Sub Folders";
+            this.chkSearchSubDirectory.UseVisualStyleBackColor = true;
+            // 
+            // chkKeepStruct
+            // 
+            this.chkKeepStruct.AutoSize = true;
+            this.chkKeepStruct.Checked = true;
+            this.chkKeepStruct.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkKeepStruct.Location = new System.Drawing.Point(669, 95);
+            this.chkKeepStruct.Name = "chkKeepStruct";
+            this.chkKeepStruct.Size = new System.Drawing.Size(232, 24);
+            this.chkKeepStruct.TabIndex = 3;
+            this.chkKeepStruct.Text = "Keep Input Folder Structure";
+            this.chkKeepStruct.UseVisualStyleBackColor = true;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(285, 195);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(179, 31);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtOutPut
+            // 
+            this.txtOutPut.Location = new System.Drawing.Point(73, 293);
+            this.txtOutPut.Multiline = true;
+            this.txtOutPut.Name = "txtOutPut";
+            this.txtOutPut.Size = new System.Drawing.Size(828, 258);
+            this.txtOutPut.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Output";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(69, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Output Format";
+            // 
+            // cmbOutputFormat
+            // 
+            this.cmbOutputFormat.FormattingEnabled = true;
+            this.cmbOutputFormat.Location = new System.Drawing.Point(227, 137);
+            this.cmbOutputFormat.Name = "cmbOutputFormat";
+            this.cmbOutputFormat.Size = new System.Drawing.Size(307, 28);
+            this.cmbOutputFormat.TabIndex = 5;
+            // 
+            // ImageConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 582);
+            this.Controls.Add(this.cmbOutputFormat);
+            this.Controls.Add(this.txtOutPut);
+            this.Controls.Add(this.chkKeepStruct);
+            this.Controls.Add(this.chkSearchSubDirectory);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnBrowseOutput);
+            this.Controls.Add(this.btnBrowseInput);
+            this.Controls.Add(this.txtOutputFolder);
+            this.Controls.Add(this.txtInputFolder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "ImageConverter";
+            this.Text = "Image Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInputFolder;
+        private System.Windows.Forms.TextBox txtOutputFolder;
+        private System.Windows.Forms.Button btnBrowseInput;
+        private System.Windows.Forms.Button btnBrowseOutput;
+        private System.Windows.Forms.CheckBox chkSearchSubDirectory;
+        private System.Windows.Forms.CheckBox chkKeepStruct;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtOutPut;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbOutputFormat;
+    }
+}
+
---
+++ ImageConverter/ImageConverter.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageConverter
+{
+    public partial class ImageConverter : Form
+    {
+        const string FILE_TYPES = ".bmp,.jpg,.jpeg,.png,.icon";
+        public enum Format
+        {
+            Jpeg,
+            Png,
+            Bmp,
+            Emf,
+            Wmf,
+            Gif,
+            Tiff,
+            Exif,
+            Icon
+        }
+        private int _sucessCount = 0;
+        public ImageConverter()
+        {
+            InitializeComponent();
+            cmbOutputFormat.DataSource = Enum.GetValues(typeof(Format));
+        }
+
+        private void btnBrowseInput_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtInputFolder.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+
+        private void btnBrowseOutput_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtOutputFolder.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInputFolder.Text))
+            {
+                MessageBox.Show("Please select input folder");
+                txtInputFolder.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(txtOutputFolder.Text))
+            {
+                MessageBox.Show("Please select output folder");
+                txtOutputFolder.Focus();
+                return;
+            }
+
+            SearchOption searchOption = chkSearchSubDirectory.Checked ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+
+            var filePaths = Directory.GetFiles(txtInputFolder.Text, "*", searchOption)
+                .Where(file => FILE_TYPES.Split(',').Any(file.ToLower().EndsWith)).ToList();
+
+            if (filePaths.Count() == 0)
+            {
+                MessageBox.Show("No images found in the directory.");
+                return;
+            }
+            _sucessCount = 0;
+            foreach (var filepath in filePaths)
+            {
+                ConvertImage(filepath);
+            }
+            
+            MessageBox.Show(string.Format("Converted {0} out of {1} images",_sucessCount,filePaths.Count()));
+        }
+
+        private void ConvertImage(string filepath)
+        {
+            try
+            {
+                using (Bitmap bmp = new Bitmap(filepath))
+                {
+                    ImageFormat format = GetImageFormat();
+                    string path = CreateDirIfNotExist(filepath);
+                    var filename = Path.GetFileName(filepath);
+                    var outfilename = Path.Combine(path, Path.GetFileNameWithoutExtension(filepath)) + "." + format.ToString();
+
+                    if (File.Exists(outfilename))
+                    {
+                        outfilename = Path.Combine(path, Path.GetFileNameWithoutExtension(filepath)) + "_" + DateTime.Now.Ticks.ToString() + "." + format.ToString();
+                    }
+
+                    bmp.Save(outfilename, format);
+                    _sucessCount++;
+                    txtOutPut.AppendText(string.Format("Converted {0} to {1}", filename, outfilename));
+                    txtOutPut.AppendText(Environment.NewLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                txtOutPut.AppendText(string.Format("Error converting {0} : {1}", filepath, ex.Message));
+                txtOutPut.AppendText(Environment.NewLine);
+            }
+        }
+
+        private string CreateDirIfNotExist(string filePath)
+        {
+            string dirPath = txtOutputFolder.Text;
+           
+            if (chkKeepStruct.Checked)
+            {
+                filePath = filePath.Replace(txtInputFolder.Text, txtOutputFolder.Text);
+                dirPath = Directory.GetParent(filePath).FullName;
+            }
+            if (!Directory.Exists(dirPath))
+                Directory.CreateDirectory(dirPath);
+            return dirPath;
+        }
+
+        public ImageFormat GetImageFormat()
+        {
+            Format format;
+            Enum.TryParse<Format>(cmbOutputFormat.SelectedValue.ToString(), out format);
+
+            if (format.Equals(Format.Jpeg))
+                return System.Drawing.Imaging.ImageFormat.Jpeg;
+            if (format.Equals(Format.Bmp))
+                return System.Drawing.Imaging.ImageFormat.Bmp;
+            if (format.Equals(Format.Png))
+                return System.Drawing.Imaging.ImageFormat.Png;
+            if (format.Equals(Format.Emf))
+                return System.Drawing.Imaging.ImageFormat.Emf;
+            if (format.Equals(Format.Exif))
+                return System.Drawing.Imaging.ImageFormat.Exif;
+            if (format.Equals(Format.Gif))
+                return System.Drawing.Imaging.ImageFormat.Gif;
+            if (format.Equals(Format.Icon))
+                return System.Drawing.Imaging.ImageFormat.Icon;
+            if (format.Equals(Format.Tiff))
+                return System.Drawing.Imaging.ImageFormat.Tiff;
+            else
+                return System.Drawing.Imaging.ImageFormat.Wmf;
+        }
+    }
+
+}
